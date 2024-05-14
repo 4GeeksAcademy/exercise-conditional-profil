66d709d49ca13fb80a3f5cb983eb4eef24346bc6
Alba <allbaluccia@gmail.com>
2024-05-14 23:25:26 +0000

    finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { left } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -33,10 +34,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>"${variables.name}, ${variables.lastName} " </h1>
+          <h3> "${variables.role} "</h3>
+          <h3>"${variables.city}, ${variables.country} "</h3>
+          <h3>"${variables.twitter}, ${variables.github}, ${variables.linkedin}, ${variables.instagram} "</h3>
+          
+          
+          
+          <ul class="${variables.socialMediaPosition} ">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
